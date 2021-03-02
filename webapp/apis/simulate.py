cc45c8875768fb0924a3651d3ead08ea37ae8c50
David Crook <dacrook@microsoft.com>
2021-03-02 09:47:51 -0500

    UI works, need to work on backend

--- webapp/apis/simulate.py
+++ webapp/apis/simulate.py
@@ -9,7 +9,22 @@ def create_leaf_device():
     simulateas a leaf device
     '''
     r_json = request.get_json()
+    print(r_json)
     # read configs
     # Deploy leafdevice .yaml to aks
-    # Write deployment information to mongo
+    # Write deployment information to sql
+    r_json["method_called"] = "leafdevice/create"
     return simplejson.dumps(r_json, ignore_nan=True)
+
+@simulate.route('/leafdevice/delete', methods=['POST'])
+def delete_leaf_device():
+    '''
+    simulateas a leaf device
+    '''
+    r_json = request.get_json()
+    print(r_json)
+    # read configs
+    # Deploy leafdevice .yaml to aks
+    # Write deployment information to sql
+    r_json["method_called"] = "leafdevice/delete"
+    return simplejson.dumps(r_json, ignore_nan=True)    
--- webapp/static/js/index.js
+++ webapp/static/js/index.js
@@ -4,26 +4,50 @@ var baseUrl = getUrl .protocol + "//" + getUrl.host;
 
 
 function activate_delete_device_modal(ko_object){
-    $("#deleteopenpositionmodal").modal("show");
-    sim_vm.staged_delete_device.device_id = ko_object.device_id;
+    $("#delete_device_modal").modal("show");
+    sim_vm.staged_delete_device.device_id(ko_object.device_id());
 }
 
 function create_leaf_devices(device_vm){
-    alert("YAY!  Its a place holder!");
+    var json_data = ko.toJSON(device_vm);
+    $.ajax(
+        {
+            url: create_device_endpoint,
+            type: "POST",
+            data: json_data,
+            contentType: "application/json",
+            success: function(data) {
+                data = JSON.parse(data);
+                toastr.success(data.method_called,"Success");
+            }
+        }
+    );
 }
 
 function delete_device(device_vm){
-    alert("Delete Device Placeholder!")
+    var json_data = ko.toJSON(device_vm);
+    $.ajax(
+        {
+            url: delete_device_endpoint,
+            type: "POST",
+            data: json_data,
+            contentType: "application/json",
+            success: function(data) {
+                data = JSON.parse(data);
+                toastr.success(data.method_called,"Success");
+            }
+        }
+    );
 }
 
 function iothub_vm(){
     var self = this;
-    self.region = "eastus";
+    self.region = ko.observable("eastus");
 }
 
 function sim_region_vm(){
     var self = this;
-    self.region = "eastus";
+    self.region = ko.observable("eastus");
 }
 
 function leaf_devices_vm(){
@@ -31,9 +55,17 @@ function leaf_devices_vm(){
     self.device_id = ko.observable("liadf9");
     self.number_devices = ko.observable(1);
     self.sim_region = ko.observable("eastus");
+    self.name = ko.observable("friendly name");
 }
 
-
+function hydrate_devices(){
+    sim_vm.devices.removeAll();
+    new_device = new leaf_devices_vm();
+    sim_vm.devices.push(new_device);
+    new_device_2 = new leaf_devices_vm();
+    new_device_2.device_id("123456");
+    sim_vm.devices.push(new_device_2);
+}
 
 function sim_manager_vm(){
     var self = this;
@@ -45,11 +77,16 @@ function sim_manager_vm(){
     self.staged_add_simregion = new sim_region_vm();
     self.staged_add_iothub = new iothub_vm();
 
+    self.devices = ko.observableArray();
+
     self.num_leaf_devices = ko.observable("loading...");
 
     self.create_leaf_devices = function() { create_leaf_devices(self.staged_add_device); };
+    self.delete_device = function() { delete_device(self.staged_delete_device); };
+    self.activate_delete_device_modal = function() { activate_delete_device_modal(this); };
 }
 
 var sim_vm = new sim_manager_vm();
 
 ko.applyBindings(sim_vm, document.getElementById("page_content"));
+hydrate_devices();
